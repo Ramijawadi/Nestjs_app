4741794a9552289e43c79b7086b90e8c19d3b66b
Jawadi Rami <67319552+Ramijawadi@users.noreply.github.com>
2024-09-21 09:48:15 +0100

    FIX : add GET ,POST ,PATCH and DELETE  method

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,41 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
+import { retry } from 'rxjs';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+
+ /*GET users/*/
+   /*GET users/:id*/
+    /*POST users/*/
+     /*PATCH users/:id*/
+
+   @Get() 
+   findAll () {
+   return []
+
+   }  
+ 
+   @Get(':id')
+   findOne(@Param ('id') id: string) {
+    return{id}
+
+   }
+
+   @Post() 
+   create(@Body() user:{}){
+   return user 
+
+}
+
+@Patch(':id')
+Update(@Param('id') id:string , @Body()  userUpdated:{}) {
+
+  return{id , ...userUpdated}
+}
+
+@Delete(':id')
+delete(@Param('id') id:string){
+return {id}
+
+}
+}
